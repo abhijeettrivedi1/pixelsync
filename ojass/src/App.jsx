db08136ed3e08107324eda72414a192f85434696
abhijeettrivedi1 <abhijeettrivedi3064@gmail.com>
2024-04-07 16:50:04 +0530

    api

--- ojass/src/App.jsx
+++ ojass/src/App.jsx
@@ -3,22 +3,48 @@ import { useState } from 'react'
 import './App.css'
 import axios from 'axios'
 function App() {
+  const [result, setresult] = useState(null)
+  const [search, setSearch] = useState("")
   async function   handleSubmit  (){
   
  const res=await axios.post('http://localhost:5001',{
       search:search
     })
     console.log(res.data.organic_results)
+    setresult(res.data.organic_results)
  }
-  const [count, setCount] = useState(0)
-  const [search, setSearch] = useState("")
+ 
   return (
 
-    <div className="container"><h1>     Search gpt</h1> <div className="search"> <div className="row"> <div className="col-md-6"> <div className="search-1"> <i className='bx bx-search-alt'></i> </div> </div> <div className="col-md-6"> <div> <div className="search-2"> <i className='bx bxs-map' ></i> <input type="text" value={search} onChange={(e) => {
+    <div className="container"><h1>     Search</h1> <div className="search"> <div className="row"> <div className="col-md-6"> <div className="search-1"> <i className='bx bx-search-alt'></i> </div> </div> <div className="col-md-6"> <div> <div className="search-2"> <i className='bx bxs-map' ></i> <input type="text" value={search} onChange={(e) => {
       setSearch(e.target.value)
-    }} placeholder="Seach what you want" /> <button onClick={handleSubmit}>Search</button> </div> </div> </div> </div> </div>
-
+    }} placeholder="Seach what you want" /> <button onClick={handleSubmit}>Search</button> </div> </div> </div> </div> 
+    
+    
+     </div>
+      
       {console.log(search)}
+    
+
+    
+    <div>
+      {result && result.map((item, index) => {
+        return (
+          <div key={index}>
+            <hr />
+            <h2>{item.title}</h2>
+            
+            <a href={`${item.link}`}><p>{item.link}</p></a>
+            
+            <p>{`${item.snippet}`} <a href={`${item.link}`}>Read more...</a></p>
+            
+            <img src={`${item.thumbnail}`} alt="" />
+            <hr />
+          </div>
+        )
+      })}
+    </div>
+
     </div>
 
   )
---
+++ ojass/src/page.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+import axios from 'axios'
+function App() {
+ 
+ 
+  return (
+    
+    <div className="container">
+  
+    
+    
+    
+
+    </div>
+
+  )
+}
+
+export default App
